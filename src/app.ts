b02ccff41df61690e28e08ebd4a651364e60dcf5
Tamu <5020311+Tamu@users.noreply.github.com>
2022-10-18 12:47:25 +0200

    Up button log

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 export class App {
   public message: string = 'Hello World!';
 
-  private emailPlaceholder: string = 'youmail@domain.com';
+  private emailPlaceholder: string = 'yourmail@domain.com';
   private email: string = undefined;
 
 
   onClick() {
-    console.log('clicked!');
+    console.log('fast-text-field', this.email);
   }
 
   emailChanged() {
     console.log('fast-text-field : changing', this.email);
-    this.message = this.email;
   }
 }
