91bfc4c36473613ab2f8a23570d7139e23ec6961
Tamu <5020311+Tamu@users.noreply.github.com>
2023-01-30 22:16:01 +0100

    build demo threejs + potree

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,37 @@
-import { inject, BindingEngine, bindable} from 'aurelia-framework'
+import { inject, BindingEngine, bindable} from 'aurelia-framework';
+import { Three } from './resources';
+
+// Potree
+import { AmbientLight, BoxGeometry,  Mesh, MeshBasicMaterial, PerspectiveCamera, Raycaster, Scene, SphereGeometry, Vector2, Vector3, WebGLRenderer } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// import { PointCloudOctree, Potree } from 'potree-core';
+
 
 @inject(Element, BindingEngine)
 export class App {
-  public message: string = 'Hello World!';
 
-  public emailPlaceholder: string = 'yourmail@domain.com';
-  public email: string = undefined;
-
+  public three: Three;
   constructor(private element: Element, private bindingEngine: BindingEngine) {
-
   }
 
-  onClick() {
-    console.log('fast-text-field', this.email);
-    this.email = 'Super !';
+  public attached(): void {
+
+      // three.js
+      
+      // this.scene.add(new AmbientLight(0xffffff));
+      
+    }
+    
+    private clic(): void {
+      
+      console.log('Goooo');
+      
+        const geometry = new BoxGeometry(35, 0.1, 35);
+        const material = new MeshBasicMaterial({color: 0x663300});
+        const cube = new Mesh(geometry, material);
+        cube.position.x = 15;
+        cube.position.y = -0.05;
+      this.three.scene.scene.add(cube);
   }
 
-  emailChanged() {
-    console.log('fast-text-field : changing', this.email);
-  }
 }
